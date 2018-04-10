bfd678ab347b9ec59466efde4309b0baa6e86104
Fatih Dalgıç <fattiihh12@gmail.com>
2018-04-10 23:12:01 +0300

    sum2

---
+++ Gezinge (Trajectory) Verisi İşleme/Gezing_İstemci/istemci dosyalar/jx browser/demo/jxbrowserdemo.sh
@@ -0,0 +1,1 @@
+java -Dfile.encoding=UTF-8 -classpath ../lib/jxbrowser-6.19.jar:../lib/jxbrowser-mac-6.19.jar:../lib/jxbrowser-linux32-6.19.jar:../lib/jxbrowser-linux64-6.19.jar:../lib/license.jar:jxbrowserdemo.jar com.teamdev.jxbrowser.chromium.demo.JxBrowserDemo
